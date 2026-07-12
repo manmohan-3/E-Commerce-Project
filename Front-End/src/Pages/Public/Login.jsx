6e8e0b1def8b5dbbb9cd7755abd94101d56e18d1
manmohan-3 <manmohanpalle3@gmail.com>
2026-07-12 15:41:42 +0530

    Added login page and also implemented validation for login pageinput fields

--- Front-End/src/Pages/Public/Login.jsx
+++ Front-End/src/Pages/Public/Login.jsx
@@ -5,20 +5,43 @@ import "./Login.css"
 
 function Login() {
     const Navigate = useNavigate();
+    const [error, seterror] = useState({ LoginId: "", Password: "" });
+
     const [formData, setformData] = useState({ LoginId: "", Password: "" });
+    const handleLogin = (event) => {
+
+        event.preventDefault();
+        const newErrors = {
+            LoginId: "", Password: ""
+        }
+        if (formData.LoginId.trim() === "") {
+            newErrors.LoginId = "Enter LoginId";
+        }
+        if (formData.Password.trim() === "") {
+            newErrors.Password = "Enter Password";
+
+        }
+        if (newErrors.LoginId || newErrors.Password) {
+            seterror(newErrors);
+            return;
+        }
+        console.log(formData);
+    }
 
     return (<>
         <div className="login-container">
             <h2>LOGIN HERE</h2>
-            <form>
+            <form onSubmit={handleLogin}>
 
                 <div className="form-group">
                     <label htmlFor="LoginId">Email or Mobile Number:</label>
-                    <input id="LoginId" type="text" value={formData.LoginId} placeholder="Enter Your Email or Mobile Number" onChange={(props) => setformData({ ...formData, LoginId: props.target.value })} />
+                    <input id="LoginId" type="text" value={formData.LoginId} placeholder="Enter Your Email or Mobile Number" onChange={(props) => {setformData({ ...formData, LoginId: props.target.value });seterror({...error,LoginId: ""});}} />
+                    <p>{error.LoginId}</p>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Enter Password:</label>
-                    <input id="password" type="password" value={formData.Password} placeholder="Enter Password" onChange={(props) => setformData({ ...formData, Password: props.target.value })} />
+                    <input id="password" type="password" value={formData.Password} placeholder="Enter Password" onChange={(props) => {setformData({ ...formData, Password: props.target.value });seterror({...error,Password:""});}} />
+                    <p>{error.Password}</p>
                 </div>
                 <button className="login-btn">Login</button>
                 <div className="signup-link">
